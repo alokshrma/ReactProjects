b49322bf333e3229f967b9691421cf37c45a3126
Alok Sharma <aloksharma861@gmail.com>
2022-01-24 18:36:06 +0530

    modified in screens1.js

---
+++ plant2RN/screen/screens.js
@@ -0,0 +1,12 @@
+<TouchableOpacity style={{ flex:1}}
+onPress={()=>{console.log("Plant on Pressed")}}>
+<Image
+source={images.plant5}
+resizeMode="cover"
+styles={{
+   width:'100%',
+   height: '100%',
+   borderRadius:20
+}}
+/>
+</TouchableOpacity>
